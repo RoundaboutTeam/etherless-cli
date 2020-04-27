342191fa582fc7e0c3db660767ac80e461bc4763
Egon <egongalvani@libero.it>
2020-04-27 15:21:07 +0200

    Run command done

--- src/Command/runCommand.ts
+++ src/Command/runCommand.ts
@@ -1,5 +1,9 @@
 import { Argv } from 'yargs';
+import { ethers, Contract, Wallet, getDefaultProvider, EventFilter } from 'ethers';
+
 import Command from './command';
+
+const ESmart = require('../../contracts/EtherlessSmart.json');
 
 class ExecCommand extends Command {
   command = 'run <function_name> [params..]';
@@ -7,10 +11,31 @@ class ExecCommand extends Command {
   description = 'execute a function ';
 
   async exec(args: any) : Promise<any> {
-    console.log(`Executing function ${args.function_name}`);
-    if (args.params.length > 0) {
-      console.log(`With params: ${args.params}`);
-    }
+    // if(UserSession.getInstance().isLogged) {
+    const wallet : Wallet = new ethers.Wallet('0x326712c09375d35e396b0cd80bc7002f13cd227b70e1959686f28ca994a28635',
+      getDefaultProvider('ropsten'));
+    const contract : Contract = new ethers.Contract('0x2eB8B6049391BD385DDD59aA020e8BC85f9eF57e', ESmart.abi,
+      getDefaultProvider('ropsten')).connect(wallet);
+
+    const requestId : string = ethers.utils.bigNumberify(ethers.utils.randomBytes(4)).toString();
+    const functionName : string = args.function_name;
+    const params : string = args.params.toString();
+
+    console.log('Creating request...');
+    let tx = await contract.runFunction(functionName, params, requestId,
+      { value: ethers.utils.parseEther('0.001') });
+    await tx.wait();
+    console.log('Request done.');
+
+    // to add: filter for the request ud
+    const eventFilter : EventFilter = contract.filters.response();
+    console.log('Waiting for the result...');
+    return new Promise<string>((resolve, reject) => {
+      contract.on(eventFilter, (result, id, event) => {
+        resolve(result);
+        contract.removeAllListeners(eventFilter);
+      });
+    });
   }
 
   builder(yargs : Argv) : any {
