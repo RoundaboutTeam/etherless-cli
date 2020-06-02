0f3c52050a90787c5d6b75080136b7b5770ae4e1
feimjakupi <feimjakupi@outlook.it>
2020-06-02 12:14:22 +0200

    New functionality: Login with mnemonic,closes: #22

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import SignupCommand from './Command/SignupCommand';
 import LoginCommand from './Command/LoginCommand';
+import LoginMNCommand from './Command/LoginMnemonicCommand';
 import LogoutCommand from './Command/LogoutCommand';
 import ListCommand from './Command/ListCommand';
 import WhoAmICommand from './Command/WhoAmI';
@@ -10,7 +11,7 @@ import CommandManager from './Command/CommandManager';
 
 
 const commandTypes : Array<any> = [SignupCommand, LoginCommand,
-  LogoutCommand, ListCommand, WhoAmICommand, Init];
+  LogoutCommand, ListCommand, WhoAmICommand, Init, LoginMNCommand];
 
 commandTypes.forEach(
   (CommandType) => CommandManager.addCommand(new CommandType()),
