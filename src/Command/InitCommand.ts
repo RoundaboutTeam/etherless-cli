e85dcd806c6a8f0aca5e33fb013db078f0f2645f
Egon <egongalvani@libero.it>
2020-07-04 10:08:22 +0200

    Created init command test, close #45

--- src/Command/InitCommand.ts
+++ src/Command/InitCommand.ts
@@ -8,9 +8,7 @@ class InitCommand extends Command {
   description = 'Functionality showcase';
 
   async exec(args: any) : Promise<any> {
-    return new Promise((resolve, reject) => {
-      resolve('Welcome to ehterless-cli \nList of commands, use etherless <command name>: \n>login \n>logout \n>signup \n>whoami');
-    });
+    return 'Welcome to ehterless-cli \nList of commands, use etherless <command name>: \n>login \n>logout \n>signup \n>whoami';
   }
 
   builder(yargs : Argv) : any {
